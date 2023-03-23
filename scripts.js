64557ebaf14968655433c772ef6385c6e3f7f5c5
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2023-03-23 10:52:58 +0000

    Created magic text testing files Improved scrolling arrows js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,2 @@
-document.getElementById("down-arrow").addEventListener("click", scrollDown);
-let viewportHeight = window.innerHeight;
-
-function scrollDown() {
-    window.scrollTo(0, viewportHeight);
-}
+let scrollHeight = window.innerHeight + 10;
+document.getElementById("down-arrow").onclick = function() {scrollTo(0, viewportHeight)}
