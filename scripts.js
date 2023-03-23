430dc4395e2da111de94fb1f612e8fc929e344e9
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2023-03-23 10:54:22 +0000

    Fixed scroll

--- scripts.js
+++ scripts.js
@@ -1,2 +1,2 @@
 let scrollHeight = window.innerHeight + 10;
-document.getElementById("down-arrow").onclick = function() {scrollTo(0, viewportHeight)}
+document.getElementById("down-arrow").onclick = function() {scrollTo(0, scrollHeight)}
